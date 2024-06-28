6df6f46a2975007aa1b65760888397eabb63446a
Jeanne <jeannefernanda.m@gmail.com>
2024-06-28 19:58:27 -0400

    Padrão Singleton

---
+++ src/one/digitalinovation/gof/SingletonLazyHolder.java
@@ -0,0 +1,21 @@
+package one.digitalinovation.gof;
+
+/**
+ * Singleton LazyHolder
+ * @see <a href='https://stackoverflow.com/a/24018148'>Referência</a>
+ *
+ */
+public class SingletonLazyHolder {
+    private  static class InstanceHolder {
+        public static SingletonLazyHolder instancia = new SingletonLazyHolder();
+    }
+
+    private SingletonLazyHolder() {
+        super();
+    }
+
+    public static SingletonLazyHolder getInstancia() {
+        return InstanceHolder.instancia;
+    }
+
+}
